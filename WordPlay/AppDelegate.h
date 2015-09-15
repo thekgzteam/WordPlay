f2af712abe2f92089a717a890360117258442810
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-09-15 01:44:45 -0500

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Edil Ashimov on 9/15/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Edil Ashimov on 9/15/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
